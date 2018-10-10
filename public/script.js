4735e934dd7795aaa3604dbf113a85e3f7313973
sammer1107 <qscesz871107@gmail.com>
2018-10-10 19:24:15 +0800

    finished homework

--- public/script.js
+++ public/script.js
@@ -1,5 +1,48 @@
 $(document).ready(function(){
-  $("#list_btn").click(function(){
-    
+  // list students
+  $("#list_btn").click(function(event){
+    event.preventDefault();
+    $.ajax({
+      method: "get",
+      url: "/list_student",
+      success: function(data){
+        console.log(data);
+        $("#list_student").html(data);
+      }
+    });
+  });
+
+  // search students
+  $("#search_btn").click(function(event){
+    event.preventDefault();
+    $.ajax({
+      method: "get",
+      url: "/search",
+      data: {"search_id": $("input[name='search_id']").val()},
+      success: function(data){
+        console.log(data);
+        $("#search_result").html(data);
+      }
+    })
+  });
+
+  // add student
+  $("#add_btn").click(function(event){
+    event.preventDefault();
+    // console.log($("input[name='new_id']").val())
+    $.ajax({
+      method: "post",
+      url: "add",
+      data: {
+        "new_id": $("input[name='new_id']").val(),
+        "new_name": $("input[name='new_name']").val()
+      }
+    });
+  });
+
+  // delete student
+  $("#delete_btn").click(function(event){
+    event.preventDefault();
+    $.post("/delete", {"del_id": $("input[name='del_id']").val() });
   });
 });
--- server.js
+++ server.js
@@ -1,12 +1,47 @@
 const express = require('express')
+const fs = require('fs')
 const app = express()
-const port = 11107
+const port = 11007
+const bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+})); 
 
+var students_str = fs.readFileSync('./students.json', 'utf-8')
+var students = JSON.parse(students_str)
 
 app.use(express.static(__dirname + '/public'))
 
 app.get('/search', function(req, res){
-  res.send(`<h2>Search result for ${req.query.search_id}:</h2>`)
+  var search_id = req.query.search_id 
+  res.send(students[search_id])
+});
+
+app.get('/list_student', function(req, res){
+  // console.log(students_str);
+  pretty_str = students_str.substring(1, students_str.length-1).split(',').join('<br>') 
+  res.send(pretty_str)
+});
+
+app.post('/add', function(req,res){
+  students[req.body.new_id] = req.body.new_name;
+  students_str = JSON.stringify(students);
+  fs.writeFile("students.json", students_str, function(err){
+    if (err){
+      console.log(err);
+    }
+  })
+});
+
+app.post('/delete', function(req, res){
+  delete students[req.body.del_id];
+  students_str = JSON.stringify(students);
+  fs.writeFile("students.json", students_str, function(err){
+    if (err){
+      console.log(err);
+    }
+  });
 })
 
 app.listen(port)
